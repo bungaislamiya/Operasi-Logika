73239cb3baec9c133d9acbf5a619253f481799de
Bunga Islamiya Putri <97377281+bungaislamiya@users.noreply.github.com>
2022-01-11 21:31:50 +0630

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+/*
+Nama    : Bunga Islamiya Putri
+NPM     : 20191310053
+latihan Bab 2
+*/
+package com.company;
+
+public class main {
+
+    public static void main(String[] args) {
+        boolean A=true, B=false;
+
+        boolean o_and = A && B;
+        boolean o_or = A || B;
+        boolean o_notA = !A;
+        boolean o_notB =!B;
+
+        System.out.println(A+" And "+B+"=>"+o_and);
+        System.out.println(A+" OR "+B+"=>"+o_or);
+        System.out.println("NOT "+A+"=>"+o_notA);
+        System.out.println("NOT "+B+"=>"+o_notB);// TODO code application logic here
+    }
